e162b1b6426d7f0f8042a4b14e96992720fb821a
Alex <alex.salerno@me.com>
2025-07-02 22:48:19 -0700

    rename to main

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    // 1. detect, validate, and parse configuration file
+    //    - Yaml.V3 for validation
+    //    - Viper for parsing and use
+    // 2. Build structure from configuration file, cache
+    // 3. Execute commands
+    fmt.Println("Hello, world.")
+}
