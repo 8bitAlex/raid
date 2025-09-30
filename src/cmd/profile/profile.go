c3e03613d7e39089a73566322450c50246b880a7
Alex <alex.salerno@me.com>
2025-09-29 18:21:24 -0700

    remove use command

--- src/cmd/profile/profile.go
+++ src/cmd/profile/profile.go
@@ -2,6 +2,7 @@ package profile
 
 import (
 	"fmt"
+	"os"
 
 	pro "github.com/8bitalex/raid/src/raid/profile"
 	"github.com/spf13/cobra"
@@ -10,7 +11,6 @@ import (
 func init() {
 	Command.AddCommand(AddProfileCmd)
 	Command.AddCommand(ListProfileCmd)
-	Command.AddCommand(UseProfileCmd)
 	Command.AddCommand(RemoveProfileCmd)
 }
 
@@ -18,13 +18,24 @@ var Command = &cobra.Command{
 	Use:     "profile",
 	Aliases: []string{"p"},
 	Short:   "Manage raid profiles",
-	Args:    cobra.NoArgs,
+	Args:    cobra.RangeArgs(0, 1),
 	Run: func(cmd *cobra.Command, args []string) {
-		profile := pro.Get()
-		if !profile.IsZero() {
-			fmt.Println(profile.Name)
+		if len(args) == 0 {
+			profile := pro.Get()
+			if !profile.IsZero() {
+				fmt.Println(profile.Name)
+			} else {
+				fmt.Println("No active profile found. Use 'raid profile use <profile>' to set one.")
+			}
+		} else if len(args) == 1 {
+			name := args[0]
+			if err := pro.Set(name); err != nil {
+				fmt.Printf("Profile '%s' not found. Use 'raid profile list' to see available profiles.\n", name)
+				os.Exit(1)
+			}
+			fmt.Printf("Profile '%s' is now active.\n", name)
 		} else {
-			fmt.Println("No active profile found. Use 'raid profile use <profile>' to set one.")
+			cmd.PrintErrln("Invalid number of arguments.")
 		}
 	},
 }
